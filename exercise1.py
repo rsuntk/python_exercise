6c100176dbf451ca8a9001f74336638f4122bfca
rsuntk <rsuntk@yukiprjkt.my.id>
2025-04-24 08:17:05 +0700

    init
    Signed-off-by: rsuntk <rsuntk@yukiprjkt.my.id>

---
+++ exercise1.py
@@ -0,0 +1,21 @@
+angka = int(input("Masukan Angka: "))
+if angka % 2 == 0:
+  print("Angka tersebut genap.")
+else:
+  print("Angka tersebut ganjil.")
+  
+input("Press to continue (Penjumlahan)")
+
+angka1 = int(input("Masukan angka pertama"))
+angka2 = int(input("Masukan angka kedua"))
+
+jumlah = angka1 + angka2
+
+print("Jumlah dari %d+%d=%d" % (angka1, angka2, jumlah))
+
+input("Press to continue (Loop)")
+
+i = 1
+while i <= 5:
+  print(i)
+  i += 1
